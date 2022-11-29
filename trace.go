a24de7d0c59484b57e1923c566e10e2524fd6125
Alex Athanasopoulos <alex@melato.org>
2022-11-29 21:29:15 +0200

    removed ".= all" from flag text.  It is displayed for invalid trace options.

--- trace.go
+++ trace.go
@@ -56,7 +56,7 @@ func T(name string, flag ...*bool) *Opt {
 
 // Flags is a convenience type that can be incorporated in flag structs to define a -t flag for trace
 type Flags struct {
-	Trace string `name:"trace" usage:"comma-separated trace options (. = all)"`
+	Trace string `name:"trace" usage:"comma-separated list of trace options"`
 }
 
 // Set is short for Set(Flags.Trace, traces)
