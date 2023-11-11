746377b0767d929a93f9e1dde968a0bf1fb2ccd7
Alex Athanasopoulos <alex@melato.org>
2023-11-11 20:44:42 +0200

    added AddTraceFuncs()

--- app.go
+++ app.go
@@ -17,6 +17,37 @@ func (t *App) AddOptionVariables(prefix string, m map[string]*bool) {
 	}
 }
 
+// AddOptionFuncs adds trace options from a map from trace names a func that enables tracing for that name.
+// If prefix is not empty, it is prepended to the name, with a "." as separator.
+func (t *App) AddOptionFuncs(prefix string, m map[string]func(bool)) {
+	for name, fn := range m {
+		if prefix != "" {
+			name = prefix + "." + name
+		}
+		t.TraceOptions = append(t.TraceOptions, &funcOption{name, fn})
+	}
+}
+
+type funcOption struct {
+	name string
+	//  The flags to set if the option is enabled
+	fn func(bool)
+}
+
+// Name is the identifier used to enable the option
+func (t *funcOption) Name() string {
+	return t.name
+}
+
+// Description is a short description that explains what enabling of this option does.  It is displayed when the options usage is displayed.
+func (t *funcOption) Description() string {
+	return ""
+}
+
+func (t *funcOption) Enable() {
+	t.fn(true)
+}
+
 func (t *App) Init() error {
 	return nil
 }
